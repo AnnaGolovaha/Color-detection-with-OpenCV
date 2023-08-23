ec5ddf0eeae34c27d016f243b981a57febf8a07d
Anna Golovaha <golovaha2011@yandex.ru>
2023-08-23 15:57:39 +0300

    add comments

--- util.py
+++ util.py
@@ -3,16 +3,16 @@ import cv2
 
 
 def get_limits(color):
-    c = np.uint8([[color]])  # BGR values
+    c = np.uint8([[color]])  # BGR значения
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    hue = hsvC[0][0][0]  # Get the hue value
+    hue = hsvC[0][0][0]  # Получаем значения hue
 
-    # Handle red hue wrap-around
-    if hue >= 165:  # Upper limit for divided red hue
+   
+    if hue >= 165:  # верхний предел для красного оттенка
         lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
         upperLimit = np.array([180, 255, 255], dtype=np.uint8)
-    elif hue <= 15:  # Lower limit for divided red hue
+    elif hue <= 15:  # нижний предел
         lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
         upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
     else:
